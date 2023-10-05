b101119412cf0315916fc98d270f01c7b83cdd9d
2002nitesh <112775047+2002nitesh@users.noreply.github.com>
2023-10-05 11:36:54 +0000

    datatypes

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3)  // we are using nodejs, not browser
+
+console.log(3
+    +
+    3) // code readability should be high
+
+console.log("Nitesh")
+
+let name = "Nitesh"
+let age = 20
+let isLoggedIn = false
+
+// number => 2 to power 53 range
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined) // type => undefined
+console.log(typeof null)  // type => object
+
