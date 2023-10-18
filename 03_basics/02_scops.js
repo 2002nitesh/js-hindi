34f47e6e8bfb3a9549f6d0d39f9303c397635500
2002nitesh <112775047+2002nitesh@users.noreply.github.com>
2023-10-18 09:33:21 +0000

    hosting

--- 03_basics/02_scops.js
+++ 03_basics/02_scops.js
@@ -1,13 +1,54 @@
 // var c = 300
-let a = 300
-if(true){
-    let a = 10
-    console.log("inner: ", a);
-    const b = 20
-    // var c = 30
-}
+// let a = 300
+// if(true){
+//     let a = 10
+//     console.log("inner: ", a);
+//     const b = 20
+//     // var c = 30
+// }
 
 
-console.log(a);   // let is a block scope
+// console.log(a);   // let is a block scope
 // console.log(b);  // const also a block scope
 // console.log(c);  // var is a globle scope
+
+
+function one(){
+    const username = "nitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+
+// one()
+
+if(true){
+    const username = "nitesh"
+    if(username === "nitesh"){
+        const website = "youtube"
+        console.log(username+website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+
+//*********** intersting ******************
+
+
+function addone(value){
+    return value + 1
+}
+
+console.log(addone(5));
+
+
+const addTwo = function(num){
+    return num + 2
+}
+
+addTwo(5)
