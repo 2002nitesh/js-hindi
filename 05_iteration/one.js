c4896e8f4e2af0e5a0884c71d3821f30c7a95ab1
2002nitesh <112775047+2002nitesh@users.noreply.github.com>
2023-10-24 05:43:36 +0000

    loops done

---
+++ 05_iteration/one.js
@@ -0,0 +1,17 @@
+// for loop
+
+// multiple select same name =>> ctrl + d
+for (let i = 0; i <= 10; i++) {
+    const element = i; 
+    console.log(element);
+}
+
+// for(){
+//     for(){
+        
+//     }
+// }
+
+// ctrl + c =>> stop infinite loop
+
+// break and continue
---
+++ 05_iteration/two.js
@@ -0,0 +1,9 @@
+// while
+let index = 0
+while (condition) {
+    
+}
+
+do {
+    
+} while (condition);
